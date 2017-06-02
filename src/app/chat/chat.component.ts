cfd2a46e6566df7d0eab73b86f2d7f98ab566996
Justin Navarro <justinn@liveanswer.com>
2017-06-02 10:11:39 -0400

    started working on the chat functionality with the sidebar

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {Router} from "@angular/router";
 import {ChatService} from "./chat.service";
 import {UsersService} from "../common/services/users.service";
 import { Users } from '../common/models/users.model';
@@ -15,7 +15,6 @@ export class ChatComponent implements OnInit {
   openProfileWindow = false;
 
   constructor(
-      private route: ActivatedRoute,
       private chatService: ChatService,
       private usersService: UsersService,
       private router: Router) { }
@@ -28,6 +27,11 @@ export class ChatComponent implements OnInit {
         this.getUsersList(user.uid);
       }
     });
+  }
+
+  isUserImageAvailable(user) {
+    var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+    return user.image != '' && user.image != null && urlregex.test(user.image);
   }
 
   logout() {
@@ -43,14 +47,19 @@ export class ChatComponent implements OnInit {
   }
 
   private getCurrentUser(uid) {
-    this.user = this.users.filter(user => {
-      return user.user_id === uid;
-    })[0];
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].user_id === uid) {
+        this.user = this.users[i];
+        this.users.splice(i, 1);
+      }
+    }
   }
 
   private getUsersList(uid) {
-    this.usersService.getAllUsers().subscribe(users => {
-      this.users = users;
+    this.usersService.getAllUsers().do(console.log).subscribe(users => {
+      this.users = users.map(user => {
+          return this.usersService.getUserImage(user);
+      });
       this.getCurrentUser(uid);
     });
   }
